e65ec95be34cdbfd28020580fa70fa9a14c661b9
polyglotpiglet <alexandra.p.batey@gmail.com>
2015-09-27 00:03:44 +0100

    it helps if the isprime function works LOL

---
+++ src/main/scala/Ten.scala
@@ -0,0 +1,10 @@
+import Utility._
+
+/**
+ * Created by alexandra on 27/09/15.
+ */
+object Ten extends App {
+
+  println ((1 to 10).filter(isPrime))
+
+}
--- src/main/scala/Utility.scala
+++ src/main/scala/Utility.scala
@@ -7,8 +7,8 @@ object Utility {
 
   def isPrime(n: Int): Boolean = {
 
-    if (n == 0 || n == 1 || n % 2 == 0) return false
     if (n == 2) return true
+    if (n == 0 || n == 1 || n % 2 == 0) return false
 
     for (i <- 3 to (pow(n, 0.5).toInt + 1) by 2) {
       if (n % i == 0) return false
