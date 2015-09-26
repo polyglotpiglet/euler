78c7db19a1837224d2a06e92445d514921458b7a
polyglotpiglet <alexandra.p.batey@gmail.com>
2015-09-26 23:42:16 +0100

    removing yucky code

--- src/main/scala/Fourteen.scala
+++ src/main/scala/Fourteen.scala
@@ -24,26 +24,23 @@ You can define a sequence with n_0 is a positive integer and given n_i, n_i+1 =
     My solution: For 1 million to 1 compute the length of the chain. Cache all intermediate results.
     Eg for 13 we would cache (13 -> 9), (40 -> 8), (20 -> 7) etc then when we hit that number we can
     just look up the length in the cache.
-    
+
  */
 
 object Fourteen extends App {
 
   val max = 1000000
 
-  var currentWinner = (0, 0)
+  val cat: IndexedSeq[(Int, Int)] = for (i <- max to 1 by -1) yield (i, computeChainLength(i))
+  println(cat.maxBy(_._2))
 
-  for (i <- max to 1 by -1) {
-    val length = computeChainLength(i)
-    if (length > currentWinner._2) currentWinner = (i, length)
-  }
-
-  println(currentWinner)
-
+  
   def computeChainLength(seed: Long): Int = {
 
+    // yes im using a var to cache the results, don't eat me
     var cache = HashMap[Long, Int]()
 
+    // helper function to compute the chain for a given number
     def computeChain(n: Long, chain: Array[Long]): (Array[Long], Int) = {
       if (cache.contains(n)) (chain, cache(n))
       if (n == 1) (chain :+ n, 0)
@@ -55,6 +52,7 @@ object Fourteen extends App {
 
     val chain = computeChain(seed, Array[Long]())
 
+    // super fun caching logic
     chain._1.reverse.zipWithIndex.foreach { case (x, i) =>
       cache += (x -> (i + chain._2 + 1)) // adding one because otherwise is indexed from 0
     }
