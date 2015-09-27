17988774edfe589c18cb06926ded5478c4275c32
polyglotpiglet <alexandra.p.batey@gmail.com>
2015-09-27 01:43:00 +0100

    more

---
+++ src/main/scala/problems/Thirteen.scala
@@ -0,0 +1,19 @@
+package problems
+
+import scala.io.Source
+
+/**
+ * Created by alexandra on 27/09/15.
+ */
+object Thirteen extends App {
+
+  val desieredNumberOfInts = 10
+
+  val bigNumbers: Array[BigInt] = Source.fromURL(getClass.getResource("/Thirteen.txt")).getLines().toArray.map(BigInt(_))
+  val sum: BigInt = bigNumbers.sum
+  println(sum)
+  val zeros = sum.toString().length - desieredNumberOfInts
+  val powerOfTen = (1 to zeros).map(_ => BigInt(10)).fold(BigInt(1)){(x,y) => x * y}
+  println((sum - (sum % powerOfTen)) / powerOfTen)
+
+}
---
+++ src/main/scala/util/DivisorUtils.scala
@@ -0,0 +1,17 @@
+package util
+import PrimeUtils._
+
+/**
+ * Created by alexandra on 27/09/15.
+ */
+object DivisorUtils {
+
+  def getDivisors(n: Int): Seq[Int] = {
+    if (isPrime(n)) return Seq(1,n)
+
+    
+
+    null
+  }
+
+}
