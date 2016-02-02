e0fc332186211b9baaef9abb9ac02eac1b0eb93f
polyglotpiglet <alexandra.p.batey@gmail.com>
2016-02-02 16:00:40 +0000

    could do this much quicker but im going skiing

---
+++ src/main/scala/problems/twenties/TwentyFour.scala
@@ -0,0 +1,11 @@
+package problems.twenties
+
+/**
+ * Created by alexandra on 02/02/16.
+ */
+object TwentyFour extends App {
+  val a = "0123456789".permutations.toArray
+  println(a(999999)) // = 2783915460
+  
+
+}
